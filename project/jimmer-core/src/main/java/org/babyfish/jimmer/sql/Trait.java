b7406ad1fe31b05d1a321780140e9c8a7988a068
honhimW <honhimw@outlook.com>
2025-08-26 16:41:52 +0800

    misc: @Trait comment

--- project/jimmer-core/src/main/java/org/babyfish/jimmer/sql/Trait.java
+++ project/jimmer-core/src/main/java/org/babyfish/jimmer/sql/Trait.java
@@ -4,8 +4,14 @@ import kotlin.annotation.AnnotationTarget;
 
 import java.lang.annotation.*;
 
+/**
+ * Allow define default method on entity, and let apt ignore it.
+ *
+ * @author honhimW
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @kotlin.annotation.Target(allowedTargets = AnnotationTarget.PROPERTY)
 @Target(ElementType.METHOD)
-public @interface Trait {}
+public @interface Trait {
+}
